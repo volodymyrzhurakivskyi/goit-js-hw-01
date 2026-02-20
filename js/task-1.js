2a1a2242b2e5dc6b4222c7fa076f4b45a2372ede
volodymyr <zhurakivskyivolodymyr@gmail.com>
2026-02-20 13:01:38 +0200

    home-work-1-js

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`)
+    return totalPrice;
+};
+
+
+
+console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
